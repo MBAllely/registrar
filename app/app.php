00e24ce93ff08b8296f1853e6cbd6a86f7b8e361
Marika Allely and Mary Warrington <marywarrington@gmail.com>
2016-03-01 11:23:39 -0800

    create initial silex routes for home, courses, and students

--- app/app.php
+++ app/app.php
@@ -1,15 +1,30 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Course.php";
+    require_once __DIR__."/../src/Student.php";
 
     $app = new Silex\Application();
-    $app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/../views'
+
+    $app['debug'] = true;
+
+    use Symfony\Component\HttpFoundation\Request;
+    Request::enableHttpMethodParameterOverride();
+
+    $server = 'mysql:host=localhost;dbname=registrar';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'
     ));
 
-    $app->get("/", function() use ($app) {
-        return $app['twig']->render('index.html.twig', array(
-            // twig input associative array
-        ));
+    $app->get("/", function() use ($app) {return $app['twig']->render('index.html.twig');
+    });
+
+    $app->get("/courses", function() use ($app) {return $app['twig']->render('courses.html.twig');
+    });
+
+    $app->get("/students", function() use ($app) {return $app['twig']->render('students.html.twig');
     });
 
     return $app;

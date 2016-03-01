4532d68b5c8f4a500c73d421966a29c1a28779c3
Marika Allely and Mary Warrington <marywarrington@gmail.com>
2016-03-01 09:04:39 -0800

    Add save, getall, deleteall functions to Student

--- src/Student.php
+++ src/Student.php
@@ -11,7 +11,7 @@ class Student
         $this->date = $date;
         $this->id = $id;
     }
-    
+
     function getName()
     {
         return $this->name;
@@ -37,6 +37,32 @@ class Student
         return $this->id;
     }
 
+
+    function save()
+    {
+        $GLOBALS['DB']->exec("INSERT INTO students (name, date) VALUES ('{$this->getName()}', '{$this->getDate()}');");
+        $this->id = $GLOBALS['DB']->lastInsertId();
+    }
+
+    static function getAll()
+    {
+        $returned_students = $GLOBALS['DB']->query("SELECT * FROM students;");
+        $students = [];
+
+        foreach($returned_students as $student) {
+            $name = $student['name'];
+            $date = $student['date'];
+            $id = $student['id'];
+            $new_student = new Student($name, $date, $id);
+            array_push($students, $new_student);
+        }
+        return $students;
+    }
+
+    static function deleteAll()
+    {
+        $GLOBALS['DB']->exec("DELETE FROM students;");
+    }
 
 
 }
--- tests/StudentTest.php
+++ tests/StudentTest.php
@@ -15,6 +15,11 @@
 
     class StudentTest extends PHPUnit_Framework_TestCase
     {
+        protected function tearDown()
+        {
+            Student::deleteAll();
+        }
+        
         function test_getInfo()
         {
             // Arrange
@@ -31,6 +36,44 @@
             $this->assertEquals($name, $result1);
             $this->assertEquals($date, $result2);
             $this->assertEquals($id, $result3);
+        }
+
+        function test_save()
+        {
+            // Arrange
+            $name = "Marika";
+            $id = null;
+            $date = "1999-01-01";
+            $test_student = new Student($name, $date, $id);
+
+            // Act
+            $test_student->save();
+            $result = Student::getAll();
+
+            // Assert
+            $this->assertEquals($test_student, $result[0]);
+        }
+
+        function test_getAll()
+        {
+            // Arrange
+            $name = "Marika";
+            $id = null;
+            $date = "1999-01-01";
+            $test_student = new Student($name, $date, $id);
+            $test_student->save();
+
+            $name2 = "Mary";
+            $id2 = null;
+            $date2 = "1969-01-01";
+            $test_student2 = new Student($name2, $date2, $id2);
+            $test_student2->save();
+
+            // Act
+            $result = Student::getAll();
+
+            // Assert
+            $this->assertEquals([$test_student, $test_student2], $result);
         }
     }
  ?>

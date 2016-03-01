763dedc94fc0f51b8aabcefa3becd2ccb524328c
Marika Allely and Mary Warrington <mballely@gmail.com>
2016-03-01 11:47:57 -0800

    add individual course route

--- app/app.php
+++ app/app.php
@@ -18,13 +18,34 @@
     $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'
     ));
 
-    $app->get("/", function() use ($app) {return $app['twig']->render('index.html.twig');
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('index.html.twig');
     });
 
-    $app->get("/courses", function() use ($app) {return $app['twig']->render('courses.html.twig');
+    $app->get("/courses", function() use ($app) {
+        return $app['twig']->render('courses.html.twig', array(
+        'courses' => Course::getAll()
+        ));
     });
 
-    $app->get("/students", function() use ($app) {return $app['twig']->render('students.html.twig');
+    $app->get("/courses/{id}", function($id) use ($app) {
+        $course = Course::find($id);
+        return $app['twig']->render('course.html.twig', array(
+            'course' => $course,
+            'students' => $course->getStudents()
+        ));
+    });
+
+    $app->get("/students", function() use ($app) {
+        return $app['twig']->render('students.html.twig');
+    });
+
+    $app->post("/courses", function() use ($app) {
+        $new_course = new Course($_POST['course_name'], $_POST['course_num']);
+        $new_course->save();
+        return $app['twig']->render('courses.html.twig', array(
+            'courses' => Course::getAll()
+        ));
     });
 
     return $app;
